08472cb0c0810693eb120c56f8e5ce72b60f2ba3
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2025-12-11 18:25:44 +0530

    Queue Using Array

---
+++ QueueArr.java
@@ -0,0 +1,58 @@
+public class QueueArr {
+    static int size = 5;
+    int[] arr;
+    int front = -1;
+    int rear = -1;
+
+    QueueArr() {
+        arr = new int[size];
+    }
+
+    void enqueue (int data) {
+        if (rear == size-1) {
+            throw new IndexOutOfBoundsException("Queue is full");
+        }
+        if (front == -1 && rear == -1) {
+            front++;
+            arr[++rear] = data;
+        }
+        else arr[++rear] = data;
+    }
+
+    int dequeue() {
+        if (front == -1 ||rear == -1 || front > rear) {
+            throw new IndexOutOfBoundsException("Queue is empty");
+        }
+        return arr[front++];
+    }
+
+    void display() {
+        if (front == -1 || rear == -1 || front > rear) {
+            System.out.println("Queue is empty");
+            return;
+        }
+        System.out.print("Queue elements: ");
+        for (int i=front; i<=rear; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        QueueArr queue = new QueueArr();
+
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(30);
+        queue.enqueue(30);
+        queue.display();
+
+        System.out.println("Dequeued : " + queue.dequeue());
+        System.out.println("Dequeued : " + queue.dequeue());
+        System.out.println("Dequeued : " + queue.dequeue());
+        System.out.println("Dequeued : " + queue.dequeue());
+
+        queue.display();
+    }
+}
