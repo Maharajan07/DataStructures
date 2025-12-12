de3669fc8ea0bebf6a987a4277d68192fd79a138
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2025-12-12 19:14:06 +0530

    Queue Using Array

--- CircularQueueArr.java
+++ CircularQueueArr.java
@@ -1,67 +1,75 @@
-public class QueueArr {
-    static int size = 5;
+public class CircularQueueArr {
+    int size = 5;
     int[] arr;
     int front = -1;
     int rear = -1;
 
-    QueueArr() {
+    CircularQueueArr() {
         arr = new int[size];
     }
 
     void enqueue (int data) {
-        if (rear == size-1) {
+        if ((rear+1)%size == front) {
             throw new IndexOutOfBoundsException("Queue is full");
         }
-        if (front == -1 && rear == -1) {
-            front++;
-            arr[++rear] = data;
-            return;
+        if (front == -1) {
+            front = 0;
         }
-        arr[++rear] = data;
+        rear = (rear+1)%size;
+        arr[rear] = data;
     }
-    // Time Complexity: O(1)
 
     int dequeue() {
-        if (front == -1 ||rear == -1 || front > rear) {
+        if (front == -1) {
             throw new IndexOutOfBoundsException("Queue is empty");
         }
-        return arr[front++];
+        int temp = arr[front];
+
+        if (front == rear) {
+            front = rear = -1;
+        }
+        else {
+            front = (front+1)%size;
+        }
+        return temp;
     }
-    // Time Complexity: O(1)
 
     void display() {
-        if (front == -1 || rear == -1 || front > rear) {
+        if (front == -1) {
             System.out.println("Queue is empty");
             return;
         }
-        System.out.print("Queue elements: ");
-        for (int i=front; i<=rear; i++) {
+        System.out.println("Queue elements: ");
+        int i = front;
+        while (true) { 
             System.out.print(arr[i] + " ");
+            if (i == rear) {
+                break;
+            }
+            i = (i+1)%size;
         }
         System.out.println();
     }
-    // Time Complexity: O(n)
 
     public static void main(String[] args) {
-        QueueArr queue = new QueueArr();
+        CircularQueueArr queue = new CircularQueueArr();
 
         queue.enqueue(10);
         queue.enqueue(20);
         queue.enqueue(30);
         queue.enqueue(40);
         queue.enqueue(50);
+        queue.enqueue(60);
         queue.display();
 
         System.out.println("Dequeued : " + queue.dequeue());
         System.out.println("Dequeued : " + queue.dequeue());
         System.out.println("Dequeued : " + queue.dequeue());
         System.out.println("Dequeued : " + queue.dequeue());
-
-
         queue.display();
 
-        // queue.enqueue(40);
-        // queue.enqueue(50);
-        // queue.display();
+        queue.enqueue(60);
+        queue.enqueue(70);
+        queue.display();
     }
 }
